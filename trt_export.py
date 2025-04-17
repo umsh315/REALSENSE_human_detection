a20acbbbea5d56d372bfe48d59dc9d77b616d6fa
umsh315 <shunsuke.umezawa@orsc.co.jp>
2025-04-17 09:43:47 +0900

    Update japanese

--- trt_export.py
+++ trt_export.py
@@ -15,16 +15,16 @@ if __name__ == '__main__':
     video_model = slowfast_r50_detection(True).eval().to(device)
     dummy_input = torch.randn(1, 3, 224, 224).to(device)
 
-    # 导出为 ONNX 格式
+    # ONNX 形式でエクスポート
     onnx_path = "slowfast_r50_detection.onnx"
-    torch.onnx.export(video_model,  # 模型
-                      dummy_input,  # 模型输入
-                      "models/slowfast.onnx",  # 输出文件路径
-                      export_params=True,  # 导出模型参数
-                      opset_version=13,  # ONNX 版本
-                      do_constant_folding=True,  # 是否进行常量折叠优化
-                      input_names=['input'],  # 输入名称
-                      output_names=['output'],  # 输出名称
-                      dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}})  # 支持动态 batch size
+    torch.onnx.export(video_model,  # モデル
+                      dummy_input,  # モデル入力
+                      "models/slowfast.onnx",  # 出力ファイルパス
+                      export_params=True,  # モデルパラメータをエクスポート
+                      opset_version=13,  # ONNX バージョン
+                      do_constant_folding=True,  # 定数畳み込み最適化を行うかどうか
+                      input_names=['input'],  # 入力名称
+                      output_names=['output'],  # 出力名称
+                      dynamic_axes={'input': {0: 'batch_size'}, 'output': {0: 'batch_size'}})  # 動的バッチサイズをサポート
 
-    print(f"模型已导出为 {onnx_path}")
+    print(f"モデルは{onnx_path}にエクスポートされました")
